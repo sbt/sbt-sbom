16b58e9f138ed7a4306c9e7c5222409c429a129b
Pierre Kisters <pierrekisters@gmail.com>
2024-08-06 09:59:50 +0200

    removed unused files

--- src/main/scala/io/github/siculo/sbtbom/BomExtractorParams.scala
+++
@@ -1,9 +0,0 @@
-package io.github.siculo.sbtbom
-
-import org.cyclonedx.Version
-import sbt.Configuration
-
-case class BomExtractorParams(
-                               schemaVersion: Version,
-                               configuration: Configuration
-                             )
