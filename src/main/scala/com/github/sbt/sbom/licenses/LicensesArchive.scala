7b63539bfb4e84572ca42815d28563f87fd149d7
Pierre Kisters <1524059+lhns@users.noreply.github.com>
2024-12-09 12:09:06 +0100

    update licenses.json and parsing without xml conversion (#94)
    * get rid of license to xml conversion step to make updates to licenses easier
    
    * updated licenses.json
    
    * make license search unambiguous and normalize ids
    
    * normalize www.
    
    * only normalize x.0 versions with end anchor
    
    * fix imports

--- src/main/scala/com/github/sbt/sbom/licenses/LicensesArchive.scala
+++ src/main/scala/com/github/sbt/sbom/licenses/LicensesArchive.scala
@@ -1,23 +1,53 @@
 package com.github.sbt.sbom.licenses
 
+import com.github.sbt.sbom.licenses.LicensesArchive.{ normalizeId, normalizeUrl }
+
 import scala.io.Source
 
 class LicensesArchive(licenses: Seq[License]) {
-  private val licensesByUrl: Map[String, License] = licenses.foldLeft(Map[String, License]()) { (map, license) =>
-    map ++ license.references.foldLeft(Map[String, License]()) { (map, ref) =>
-      map + (ref -> license)
-    }
-  }
+  private val licensesByNormalizedUrl: Map[String, Seq[License]] =
+    licenses.iterator
+      .flatMap { license =>
+        license.references.map { reference =>
+          (normalizeUrl(reference), license)
+        }
+      }
+      .toList
+      .groupBy(_._1)
+      .mapValues(_.map(_._2))
 
-  def findByUrl(url: String): Option[License] = licensesByUrl.get(url)
+  private val licenseByNormalizedId: Map[String, License] =
+    licenses
+      .groupBy(license => normalizeId(license.id))
+      .mapValues {
+        case Seq(license) => license
+        case licenses     => throw new RuntimeException(s"conflicting licenses: $licenses")
+      }
 
-  def findById(id: String): Option[License] = licenses.find(_.id.contains(id))
+  def findByUrl(url: String): Seq[License] = licensesByNormalizedUrl.getOrElse(normalizeUrl(url), Seq.empty)
+
+  def findById(id: String): Option[License] = licenseByNormalizedId.get(normalizeId(id))
 }
 
 object LicensesArchive {
-  private lazy val fileStream = getClass.getResourceAsStream("/licenses.xml")
-  private lazy val archiveText = Source.fromInputStream(fileStream).mkString
-  private lazy val archive = new LicensesArchive(new LicensesArchiveParser(archiveText).licenses)
+  private def normalizeUrl(url: String): String = url.toLowerCase
+    .replaceFirst("^https?://(www\\.)?", "https://")
+    .replaceFirst("/$", "")
+    .replaceFirst("\\.(html|txt)$", "")
 
-  def findByUrl(url: String): Option[License] = archive.findByUrl(url)
+  // Apache-2.0 will be normalized to apache 2, BSD 3-Clause will be normalized to bsd 3 clause
+  private def normalizeId(id: String): String = id.toLowerCase
+    .replace("-", " ")
+    .replaceFirst("(?<=\\d)\\.0$", "")
+
+  private def loadResourceAsString(resource: String): String = {
+    val fileStream = getClass.getResourceAsStream(resource)
+    Source.fromInputStream(fileStream).mkString
+  }
+
+  def fromJsonString(json: String): LicensesArchive =
+    new LicensesArchive(LicensesArchiveJsonParser.parseString(json))
+
+  lazy val bundled: LicensesArchive =
+    fromJsonString(loadResourceAsString("/licenses.json"))
 }
---
+++ src/main/scala/com/github/sbt/sbom/licenses/LicensesArchiveJsonParser.scala
@@ -0,0 +1,44 @@
+package com.github.sbt.sbom.licenses
+
+import io.circe.Decoder
+import io.circe.generic.semiauto.deriveDecoder
+import io.circe.parser._
+
+import scala.util.control.NonFatal
+
+private[licenses] object LicensesArchiveJsonParser {
+  private case class LicenseJson(
+      licenseId: String,
+      name: String,
+      seeAlso: Seq[String]
+  )
+
+  private object LicenseJson {
+    implicit val decoder: Decoder[LicenseJson] = deriveDecoder
+  }
+
+  private case class LicensesArchiveJson(
+      licenses: Seq[LicenseJson]
+  )
+
+  private object LicensesArchiveJson {
+    implicit val decoder: Decoder[LicensesArchiveJson] = deriveDecoder
+  }
+
+  private def licenseFromLicenseEntry(licenseEntry: LicenseJson): License = License(
+    id = licenseEntry.licenseId,
+    name = licenseEntry.name,
+    references = licenseEntry.seeAlso
+  )
+
+  def parseString(string: String): Seq[License] = {
+    val licensesArchiveJson =
+      try {
+        decode[LicensesArchiveJson](string).toTry.get
+      } catch {
+        case NonFatal(e) => throw new RuntimeException("failed to parse licenses archive json", e)
+      }
+
+    licensesArchiveJson.licenses.map(licenseFromLicenseEntry)
+  }
+}
