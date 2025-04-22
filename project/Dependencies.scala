a0ab547c8daebe475e2956b731a64a0c017c8d97
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2025-04-22 20:29:40 +0200

    Update circe-generic, circe-parser to 0.14.13 (#141)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,8 +6,8 @@ import sbt._
 
 object Dependencies {
   lazy val library = Seq(
-    "io.circe"      %% "circe-generic"       % "0.14.12",
-    "io.circe"      %% "circe-parser"        % "0.14.12",
+    "io.circe"      %% "circe-generic"       % "0.14.13",
+    "io.circe"      %% "circe-parser"        % "0.14.13",
     "org.cyclonedx"  % "cyclonedx-core-java" % "10.2.1",
     "org.scalatest" %% "scalatest"           % "3.2.19" % Test,
     "org.scalamock" %% "scalamock"           % "7.3.1"  % Test
