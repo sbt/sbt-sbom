3f041d8e8cfa230e95f6fa5604d576fc7433a406
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2025-07-04 06:52:53 +0200

    Update scalamock to 7.4.0 (#160)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,6 +10,6 @@ object Dependencies {
     "io.circe"      %% "circe-parser"        % "0.14.14",
     "org.cyclonedx"  % "cyclonedx-core-java" % "10.2.1",
     "org.scalatest" %% "scalatest"           % "3.2.19" % Test,
-    "org.scalamock" %% "scalamock"           % "7.3.3"  % Test
+    "org.scalamock" %% "scalamock"           % "7.4.0"  % Test
   )
 }
