247f286887b9d18ddb6ad598a90ec4b0fec99332
Scala Steward <scala_steward@virtuslab.com>
2025-09-29 16:47:23 +0000

    Update cyclonedx-core-java to 11.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
   lazy val library = Seq(
     "io.circe"      %% "circe-generic"       % "0.14.14",
     "io.circe"      %% "circe-parser"        % "0.14.14",
-    "org.cyclonedx"  % "cyclonedx-core-java" % "10.2.1",
+    "org.cyclonedx"  % "cyclonedx-core-java" % "11.0.0",
     "org.scalatest" %% "scalatest"           % "3.2.19" % Test,
     "org.scalamock" %% "scalamock"           % "7.5.0"  % Test
   )
